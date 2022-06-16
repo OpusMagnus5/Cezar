1339ad7db6c621e908d7ffe78b8707ff03a88e08
OpusMagnus5 <damianbodzioch95@gmail.com>
2022-06-16 18:19:35 +0200

    new

---
+++ src/main/java/com/damian/bodzioch/Alphabet.java
@@ -0,0 +1,32 @@
+package com.damian.bodzioch;
+
+import java.util.ArrayList;
+
+public class Alphabet {
+    public ArrayList<Character> letters = new ArrayList<>();
+
+
+    private static Alphabet alphabet = new Alphabet();
+
+    private Alphabet(){
+        for (int i = 65; i <= 90; i++){
+            if (i == 'Q' || i == 'V' || i == 'X'){
+                continue;
+            }
+            this.letters.add((char) i);
+        }
+        this.letters.add(1, 'Ą');
+        this.letters.add(4, 'Ć');
+        this.letters.add(7, 'Ę');
+        this.letters.add(15, 'Ł');
+        this.letters.add(18, 'Ń');
+        this.letters.add(20, 'Ó');
+        this.letters.add(24, 'Ś');
+        this.letters.add('Ź');
+        this.letters.add('Ż');
+    }
+
+    public static Alphabet getAlphabet() {
+        return alphabet;
+    }
+}
---
+++ src/main/java/com/damian/bodzioch/Main.java
@@ -0,0 +1,39 @@
+package com.damian.bodzioch;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Podaj treść do zaszyfrowania: ");
+        String input = in.nextLine();
+
+        System.out.print("Podaj współczynnik przesunięcia: ");
+        int displacementFactor = in.nextInt();
+
+        System.out.println(encrypt(input, displacementFactor));
+    }
+
+    public static String encrypt(String input, int displacementFactor){
+        int index;
+        int indexNewLetter;
+        String output = "";
+        for (int i = 0; i < input.length(); i++){
+            index = Alphabet.getAlphabet().letters.indexOf(Character.toUpperCase(input.charAt(i)));
+            if (index + displacementFactor >= Alphabet.getAlphabet().letters.size()) {
+                indexNewLetter = index + displacementFactor - Alphabet.getAlphabet().letters.size() - 1;
+            } else if (index + displacementFactor < 0) {
+                indexNewLetter = index + displacementFactor + Alphabet.getAlphabet().letters.size() - 1;
+            } else {
+                indexNewLetter = index + displacementFactor;
+            }
+            if (Character.isUpperCase(input.charAt(i))){
+                output += Alphabet.getAlphabet().letters.get(indexNewLetter);
+            } else {
+                output += Character.toLowerCase(Alphabet.getAlphabet().letters.get(indexNewLetter));
+            }
+        }
+        return output;
+    }
+}
